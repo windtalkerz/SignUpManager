120cc136dc4924fe01480a8a9961b1e2a795de5e
Marc Ortlieb <windtalkerz@me.com>
2016-04-13 14:28:17 +0200

    Project initialized

---
+++ SignUpManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignUpManager
+//
+//  Created by Marc Ortlieb on 13.04.16.
+//  Copyright © 2016 Marc Ortlieb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
